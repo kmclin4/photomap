4ccc483648ee41da6bb85f638533e0baac0da3ac
kmclin4 <98778367+kmclin4@users.noreply.github.com>
2022-04-13 23:16:30 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -20,7 +20,7 @@ function createPhotoMap () {
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
   var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
+	maxZoom: 13,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       
   }).addTo(map);
